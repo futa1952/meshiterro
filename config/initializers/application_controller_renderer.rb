269f90d5c20865456cd8949d1034800d591ef202
futa1952 <futa1952@icloud.com>
2020-07-09 16:50:20 +0900

    [add] 投稿画像の詳細ページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '22d387dd8c2445164ec32abe03e522b2c68504e10a9b4afe4bfea9a1d61c10e662d21e69749628097b1923cb698c57e9eedccd2d7cabc7ce1fb5767c25c17204'
